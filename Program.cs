f75e6d205270f25e143be278488e8846504db738
gustavobrazon <92831689+gustavobrazon@users.noreply.github.com>
2021-10-19 19:54:01 -0400

    Entrega Examen PROG1. Gustavo Brazon.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "3";
+            String b = "2";
+            String c = "8";
+
+            String combinacion1 = a + a + c + b;
+            String combinacion2 = b + c + a;
+            String combinacion3 = b + c + b + c;
+
+            String msg1 = "aacb: " + combinacion1;
+            String msg2 = "bca: " + combinacion2;
+            String msg3 = "bcbc: " + combinacion3;
+
+            Console.WriteLine(msg1);
+            Console.WriteLine(msg2);
+            Console.WriteLine(msg3);
+        }
+    }
+}
